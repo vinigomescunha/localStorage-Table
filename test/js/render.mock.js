890e9cead8bd49136efbcca42e01357136f055cc
Vinicius Gomes da Cunha <vinigomescunha@gmail.com>
2017-07-20 23:39:36 -0300

    add tests

---
+++ test/js/render.mock.js
@@ -0,0 +1,9 @@
+var Render = function () {
+    this.renderTable = function () {};
+    this.enablePrev = function () {};
+    this.disablePrev = function () {};
+    this.enableNext = function () {};
+    this.disableNext = function () {};
+    this.renderTotal = function () {};
+    this.event = function () {};
+};
---
+++ test/js/test.js
@@ -0,0 +1,28 @@
+var key = 'test';
+var s = new Storage(key);
+var l = Lipsum;
+var u = QUnit;
+var tb = new Table({
+    id: 'mock'
+});
+u.test("Storage: key is same that defined...", function (assert) {
+    assert.equal(s.key, key);
+});
+u.test("Storage: set array, get expect array...", function (assert) {
+    s.set([key]); // only array
+    assert.equal(s.get(), key);
+});
+u.test("Storage: set non-array, get expect empty array...", function (assert) {
+    s.set(key); // empty array as return
+    assert.equal(s.get().length, 0);
+});
+u.test("Lipsum: lipsum is defined...", function (assert) {
+    assert.notEqual(l.lipsum, '');
+});
+u.test("Table: lipsum var is defined...", function (assert) {
+    var spy = sinon.spy();
+    l.random = spy;
+    tb.addItem();
+    // sinon.assert.called(spy);
+    assert.equal(spy.called, true)
+});
